26feb2ca574c3aabd62edb5097ec208cde4976e5
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-02-25 20:51:54 +0200

    added controllers

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,43 @@
-import contactsService from "../services/contactsServices.js";
+import contactsServices from "../services/contactsServices.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsServices.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsServices.getContactById(id);
+    if (!result) {
+      const error = new Error(`Contact with id=${id} not found`);
+      error.status = 404;
+      throw error;
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = (req, res, next) => {};
