2b324af3d9a5622067df5b13f9dea2c8d52aec65
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2024-03-11 13:47:28 +0200

    fix

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,13 +1,14 @@
 import jwt from "jsonwebtoken";
-import HttpError from "../helpers/HttpError.js";
+
 import { findUserById } from "../services/userServices.js";
-import dotenv from "dotenv";
+
+import HttpError from "../helpers/HttpError.js";
 
 const { JWT_SECRET } = process.env;
 
-export const authenticate = async (req, res, next) => {
-  const { authorization } = req.header;
-  const [bearer, token] = authorization.split(" ");
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  const [bearer, token] = authorization.split("");
   if (bearer !== "Bearer") {
     return next(HttpError(401, "Not authorized"));
   }
@@ -20,6 +21,8 @@ export const authenticate = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
-    next(HttpError(401, "Not authorized"));
+    next(HttpError(401, error.message));
   }
 };
+
+export default authenticate;
